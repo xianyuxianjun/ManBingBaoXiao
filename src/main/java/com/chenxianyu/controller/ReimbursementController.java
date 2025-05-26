2cfdeb37d9a99adf41bc5a96d0d8ea4bb69c8b1f
chenxianyu <1796792003@qq.com>
2025-05-26 17:15:16 +0800

    报销信息管理

--- src/main/java/com/chenxianyu/controller/ReimbursementController.java
+++ src/main/java/com/chenxianyu/controller/ReimbursementController.java
@@ -1,12 +1,12 @@
 package com.chenxianyu.controller;
 
+import ch.qos.logback.core.util.StringUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.chenxianyu.entity.Reimbursement;
 import com.chenxianyu.entity.Reslut;
 import com.chenxianyu.service.IReimbursementService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -43,7 +43,48 @@ public class ReimbursementController {
             @RequestParam(required = false) String isReimbursement,
             @RequestParam(required = false) String isRemit
     ){
+        Page<Reimbursement> page = reimbursementService.lambdaQuery()
+                .like(StringUtil.notNullNorEmpty(fullName), Reimbursement::getFullName, fullName)
+                .eq(StringUtil.notNullNorEmpty(medicalName), Reimbursement::getMedicalName, medicalName)
+                .eq(StringUtil.notNullNorEmpty(invoiceNo), Reimbursement::getInvoiceNo, invoiceNo)
+                .eq(StringUtil.notNullNorEmpty(isReimbursement), Reimbursement::getIsReimbursement, isReimbursement)
+                .eq(StringUtil.notNullNorEmpty(isRemit), Reimbursement::getIsRemit, isRemit)
+                .page(new Page<>(current, size));
+
+        return Reslut.Success(page);
+    }
+
+    @PostMapping("/save")
+    public Reslut save(@RequestBody Reimbursement reimbursement){
+        boolean isSuccess = reimbursementService.save(reimbursement);
+        if (!isSuccess) {
+            return Reslut.Fail("保存失败");
+        }
+        return Reslut.Success();
 
     }
 
+    @PostMapping("/update")
+    public Reslut update(@RequestBody Reimbursement reimbursement){
+        boolean isSuccess = reimbursementService.updateById(reimbursement);
+        if (!isSuccess) {
+            return Reslut.Fail("修改失败");
+        }
+        return Reslut.Success();
+    }
+
+    @DeleteMapping("/delete/{reimbursementId}")
+    public Reslut delete(@PathVariable Long reimbursementId){
+        boolean isSuccess = reimbursementService.removeById(reimbursementId);
+        if (!isSuccess) {
+            return Reslut.Fail("删除失败");
+        }
+        return Reslut.Success();
+    }
+
+    @GetMapping("/get/{reimbursementId}")
+    public Reslut get(@PathVariable Long reimbursementId){
+        Reimbursement reimbursement = reimbursementService.getById(reimbursementId);
+        return Reslut.Success(reimbursement);
+    }
 }
