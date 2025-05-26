d43d485fec0be8c7ab42e51990cd2b4be706f901
chenxianyu <1796792003@qq.com>
2025-05-26 09:25:45 +0800

    区域管理1.0

--- src/main/java/com/chenxianyu/controller/DivisionController.java
+++ src/main/java/com/chenxianyu/controller/DivisionController.java
@@ -1,6 +1,12 @@
 package com.chenxianyu.controller;
 
+import com.chenxianyu.entity.Insuerd;
+import com.chenxianyu.entity.Reslut;
+import com.chenxianyu.service.IDivisionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,5 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/division")
 public class DivisionController {
+
+    @Autowired
+    private IDivisionService divisionService;
+
+    @GetMapping("/list")
+    public Reslut list(
+            @RequestParam(defaultValue = "1") Integer current,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String level
+            ){
+
+    }
+
 
 }
