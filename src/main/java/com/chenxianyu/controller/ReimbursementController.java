5dc8b6d539fb4a6278898d72585c181803e8ba0f
xianyuxianjun <1796792003@qq.com>
2025-05-26 16:41:30 +0800

    报销信息管理1.0

--- src/main/java/com/chenxianyu/controller/ReimbursementController.java
+++ src/main/java/com/chenxianyu/controller/ReimbursementController.java
@@ -1,6 +1,11 @@
 package com.chenxianyu.controller;
 
+import com.chenxianyu.entity.Reslut;
+import com.chenxianyu.service.IReimbursementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,5 +19,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/reimbursement")
 public class ReimbursementController {
+
+    @Autowired
+    private IReimbursementService reimbursementService;
+
+    /**
+     * @param current 当前页
+     * @param size 每页大小
+     * @param fullName 姓名
+     * @param medicalName 疾病名称
+     * @param invoiceNo 发票号
+     * @param isReimbursement 是否报销
+     * @param isRemit 是否汇款
+     * @return
+     */
+    @GetMapping("/list")
+    public Reslut list(
+            @RequestParam(defaultValue = "1") Integer current,
+            @RequestParam(defaultValue = "10") Integer size,
+            @RequestParam(required = false) String fullName,
+            @RequestParam(required = false) String medicalName,
+            @RequestParam(required = false) String invoiceNo,
+            @RequestParam(required = false) String isReimbursement,
+            @RequestParam(required = false) String isRemit
+    ){
+
+    }
 
 }
--- src/main/java/com/chenxianyu/entity/Reimbursement.java
+++ src/main/java/com/chenxianyu/entity/Reimbursement.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 import lombok.ToString;
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author chenxianyu
@@ -20,23 +20,53 @@ public class Reimbursement implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    /**
+     * ID
+     */
     private Long id;
 
+    /**
+     * 姓名
+     */
     private String fullName;
 
+    /**
+     * 疾病名称
+     */
     private String medicalName;
 
+    /**
+     * 居住地址
+     */
     private String address;
 
-    private String reimbursementAmount;
+    /**
+     * 报销金额
+     */
+    private Double reimbursementAmount;
 
-    private String totalTost;
+    /**
+     * 总计
+     */
+    private Double totalTost;
 
-    private Integer invoiceNo;
+    /**
+     * 发票号
+     */
+    private String invoiceNo;
 
+    /**
+     * 报销时间
+     */
     private LocalDate date;
 
+    /**
+     * 是否审核
+     */
     private String isReimbursement;
 
+    /**
+     * 是否汇款
+     */
     private String isRemit;
 }
